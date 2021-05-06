05c8e8d12a0fddd963efe0be3292169f107fcb9b
Ochirgarid Chinzorig <ochir.garid@gmail.com>
2021-05-06 01:10:25 -0500

    assignment-9 prob 1-3

---
+++ CS435/Assignment-9/lab8_graph2/ShortestPathLength.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class ShortestPathLength extends BreadthFirstSearch {
+    private HashMap<Vertex, Integer> vertexLevel;
+	private int currentLevel;
+	public ShortestPathLength(Graph graph) {
+		super(graph);
+	}
+	protected void processVertex(Vertex w) {
+		vertexLevel.put(w, currentLevel);
+        currentLevel ++;
+	}
+
+	public int shortestPathLength(Vertex u, Vertex v) {
+        currentLevel = 0;
+        vertexLevel = new HashMap<>();
+		start(u);
+		return vertexLevel.get(v);
+	}
+
+}
